afc85a69778f8b00a64eddcb10c638f70a393821
imbenjames <imbenjames@gmail.com>
2018-08-14 19:47:10 -0500

    Finished ch 3

--- Exercise Files/Ch3/calendars_start.py
+++ Exercise Files/Ch3/calendars_start.py
@@ -3,21 +3,30 @@
 #
 
 # import the calendar module
-
+import calendar
 
 # create a plain text calendar
-
+c = calendar.TextCalendar(calendar.SUNDAY)
+# st = c.formatmonth(2017, 1, 0, 0)
+# print(st)
 
 # create an HTML formatted calendar
-
+# hc = calendar.HTMLCalendar(calendar.SUNDAY)
+# st = hc.formatmonth(2017, 1)
+# print(st)
 
 # loop over the days of a month
 # zeroes mean that the day of the week is in an overlapping month
-
+# for i in c.itermonthdays(2017, 8):
+#     print(i)
   
 # The Calendar module provides useful utilities for the given locale,
 # such as the names of days and months in both full and abbreviated forms
+for name in calendar.month_name:
+    print(name)
 
+for day in calendar.day_name:
+    print(day)
 
 # Calculate days based on a rule: For example, consider
 # a team meeting on the first Friday of every month.
